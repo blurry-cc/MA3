ddb579c313d4092a27c4bed11739c7789e0ab014
blurry-cc <enna.kadunic20@gmail.com>
2025-10-03 08:26:03 +0200

    changed approximate_pi. much faster now. like MUCH faster

--- MA3.py
+++ MA3.py
@@ -17,22 +17,25 @@ import functools # for exc 1.2
 
 #random.seed(42) # make results reproducible
 
-def approximate_pi(n): # Ex1
-    #n is the number of points
-    # Write your code here
-    points = [[random.uniform(-1, 1), random.uniform(-1, 1)] for _ in range(n)] # generates n random points 
-    nc = [(x, y) for x, y in points if x**2 + y**2 < 1] # all points inside circle 
-    ns = [(x, y) for x, y in points if (x, y) not in nc] # all points outside circle
+def approximate_pi(n): 
+    nc_x, nc_y = [], []
+    ns_x, ns_y = [], []
 
-    # separate to x, y lists using unpacking. add fallback arguments in case a list is empty (probably no risk but...)
-    nc_x, nc_y =  zip(*nc) if nc else ([], []) # else unpacks emtpy list into two empty lists 
-    ns_x, ns_y = zip(*ns) if ns else ([], []) 
-    
-    plt.scatter(nc_x, nc_y, c='red')
+    for _ in range(n): 
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        if x**2 + y**2 < 1: 
+            nc_x.append(x)
+            nc_y.append(y)
+        else: # then outside circle 
+            ns_x.append(x)
+            ns_y.append(y)
+
+    plt.scatter(nc_x, nc_y, c='red') #plot points inside circle in red
     plt.scatter(ns_x, ns_y, c='blue')
-    plt.savefig(f"approx-pi_{n}")
+    plt.savefig(f'approx-pi_{n}')
 
-    return round(4*len(nc)/n, 6)
+    return round(4*len(nc_x)/n, 6) #len(nc_x) = len(nc_y) = n.o. of points inside circle. 
 
 def sphere_volume(n, d): #Ex2, approximation
     #n is the number of points
@@ -74,10 +77,11 @@ def sphere_volume_parallel2(n,d,np=10):
                         # equal to 1 process of size n. 
     
 def main():
-    # #Ex1
-    # dots = [1000, 10000, 100000]
-    # for n in dots:
-    #     approximate_pi(n)
+    #Ex1
+    print("Testing old approximate pi.")
+    dots = [1000, 10000, 100000]
+    for n in dots:
+        print(approximate_pi(n))
     # #Ex2
     # n = 100000
     # d = 2
@@ -89,21 +93,21 @@ def main():
     # sphere_volume(n,d)
     # print(f"Actual volume of {d} dimentional sphere = {hypersphere_exact(d)} m^{d} ")
 
-    #Ex3
-    n = 100000
-    d = 11
-    print("Testing excercise 3\n")
-    start = pc()
-    for y in range (10):
-        sphere_volume(n,d)
-    stop = pc()
-    print(f"Ex3: Sequential time of {d} and {n}: {round(stop-start, 3)} seconds\n")
-    print("What is parallel time?")
-    start = pc() 
-    sphere_volume_parallel1(n, d)
-    stop = pc()
-    print(f"Parallel time of {d} and {n}: {round(stop-start, 3)} seconds\n")
-    print("Test complete.")
+    # #Ex3
+    # n = 100000
+    # d = 11
+    # print("Testing excercise 3\n")
+    # start = pc()
+    # for y in range (10):
+    #     sphere_volume(n,d)
+    # stop = pc()
+    # print(f"Ex3: Sequential time of {d} and {n}: {round(stop-start, 3)} seconds\n")
+    # print("What is parallel time?")
+    # start = pc() 
+    # sphere_volume_parallel1(n, d)
+    # stop = pc()
+    # print(f"Parallel time of {d} and {n}: {round(stop-start, 3)} seconds\n")
+    # print("Test complete.")
 
     # #Ex4
     # n = 1000000
